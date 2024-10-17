5e1106cfc44398b25e44429e64d213a198c99123
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-16 19:36:14 -0500

    Se configuran rutas iniciales

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home/Home";
+import Header from "../components/Header/Header";
+import Sections from "../Pages/Sections/Sections";
+import CarShop from "../Pages/CarShop/CarShop";
 
 function AppRoutes() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/section" element={<Sections />} />
+          <Route path="/carrito" element={<CarShop />} />
         </Routes>
       </BrowserRouter>
     </>

a87d1a2a92a69f60a90765333b63974200d47e46
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-17 20:06:58 -0500

    Se configuran rutas iniciales

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,17 +3,28 @@ import Home from "../Pages/Home/Home";
 import Header from "../components/Header/Header";
 import Sections from "../Pages/Sections/Sections";
 import CarShop from "../Pages/CarShop/CarShop";
+import InfoProduct from "../Pages/InfoProduct/InfoProduct";
+import Menu from "../components/Menu/Menu";
+import { useState } from "react";
 
 function AppRoutes() {
+  const [menuStatus, setMenuStatus] = useState<boolean>(false);
+
+  const handleChangeMenuStatus = () => {
+    setMenuStatus((prev) => !prev);
+  };
+
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header handleMenuStatus={handleChangeMenuStatus} />
         <Routes>
           <Route index element={<Home />} />
           <Route path="/section" element={<Sections />} />
           <Route path="/carrito" element={<CarShop />} />
+          <Route path="/producto" element={<InfoProduct />} />
         </Routes>
+        <Menu menuStatus={menuStatus} />
       </BrowserRouter>
     </>
   );

699837eb5313f09ee08876ab7011009e3b6c3d2a
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-02 13:56:34 -0500

    Refactorización de código

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,8 +4,16 @@ import Header from "../components/Header/Header";
 import Sections from "../Pages/Sections/Sections";
 import CarShop from "../Pages/CarShop/CarShop";
 import InfoProduct from "../Pages/InfoProduct/InfoProduct";
+import Login from "../Pages/Login/Login";
 import Menu from "../components/Menu/Menu";
 import { useState } from "react";
+
+const routes = [
+  { path: "/section", element: <Sections />, allowedRoles: [] },
+  { path: "/carrito", element: <CarShop />, allowedRoles: [] },
+  { path: "/producto", element: <InfoProduct />, allowedRoles: [] },
+  { path: "/auth/login", element: <Login />, allowedRoles: [] },
+];
 
 function AppRoutes() {
   const [menuStatus, setMenuStatus] = useState<boolean>(false);
@@ -20,9 +28,9 @@ function AppRoutes() {
         <Header handleMenuStatus={handleChangeMenuStatus} />
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/section" element={<Sections />} />
-          <Route path="/carrito" element={<CarShop />} />
-          <Route path="/producto" element={<InfoProduct />} />
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
         </Routes>
         <Menu menuStatus={menuStatus} />
       </BrowserRouter>

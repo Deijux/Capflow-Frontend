3204d450228dbcae9ea57dea6648d384ac755b51
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-04 12:37:24 -0500

    Use login service

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import loginService from "../../services/LogIn";
+
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmit = async (
+    e: React.ChangeEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    e.preventDefault();
+    const role = await loginService({ username, password });
+    if (role === "ROLE_ADMIN") return navigate("/admin/dashboard");
+  };
+
   return (
     <section className="flex h-96 w-full items-center justify-center">
       <div className="flex justify-center rounded-md border py-5 shadow-md shadow-gray-300 first:w-96">
-        <form className="flex w-fit flex-col gap-3">
+        <form onSubmit={onSubmit} className="flex w-fit flex-col gap-3">
           <div className="flex flex-col gap-1">
             <label htmlFor="username">Nombre de usuario</label>
             <input
@@ -10,6 +26,7 @@ function Login() {
               type="text"
               id="username"
               placeholder="Ingrese su nombre de usuario"
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="flex flex-col gap-1">
@@ -19,6 +36,7 @@ function Login() {
               type="password"
               id="password"
               placeholder="Ingrese su contraseña"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button className="rounded bg-black py-1 text-white" type="submit">

5508746a18c6f4004f464ac9eec6c3cefabd986d
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-05 17:22:39 -0500

    Add index.ts to components and pages

---
+++ src/Pages/index.ts
@@ -0,0 +1,6 @@
+export { default as AdminRoutes } from "./Admin/AdminRoutes";
+export { default as CarShop } from "./CarShop/CarShop";
+export { default as Home } from "./Home/Home";
+export { default as InfoProduct } from "./InfoProduct/InfoProduct";
+export { default as Login } from "./Login/Login";
+export { default as Sections } from "./Sections/Sections";
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Card } from "./Card/Card.tsx";
+export { default as Header } from "./Header/Header.tsx";
+export { default as Menu } from "./Menu/Menu.tsx";

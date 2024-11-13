9eb446991360ac00fd779a4f23cb543eeba49da4
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-13 18:01:55 -0500

    Import roles types and add validation to get role

--- src/context/Global.provider.tsx
+++ src/context/Global.provider.tsx
@@ -1,14 +1,24 @@
 import { ReactNode, useState } from "react";
 import { GlobalContext } from "./Global.context";
+import { UserRole } from "../types";
 
-const EmptyGlobalState: string = localStorage.getItem("role") || "ROLE_GUEST";
+const validateUserRole = (role: string): UserRole => {
+  if (role === "ROLE_ADMIN" || role === "ROLE_USER" || role === "ROLE_GUEST") {
+    return role as UserRole;
+  }
+  return "ROLE_GUEST";
+};
+
+const EmptyGlobalState: UserRole = validateUserRole(
+  localStorage.getItem("role") || "ROLE_GUEST",
+);
 
 interface GlobalProps {
   children: ReactNode;
 }
 
 export const GlobalProvider = ({ children }: GlobalProps) => {
-  const [role, setRole] = useState<string>(EmptyGlobalState);
+  const [role, setRole] = useState<UserRole>(EmptyGlobalState);
 
   return (
     <GlobalContext.Provider value={{ role, setRole }}>

d0203cb8e75f3be90aa1489d36389487753cf482
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-12-09 16:58:12 -0500

    Add get products by id and brand services

--- src/services/Products.ts
+++ src/services/Products.ts
@@ -8,3 +8,21 @@ export const getProducts = async () => {
     throw new Error(`New error generated: ${error}`);
   }
 };
+
+export const getProductById = async (id: string | undefined) => {
+  try {
+    const response = await axiosInstance.get(`api/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`New error generated: ${error}`);
+  }
+};
+
+export const getProductByBrand = async (brand: string | undefined) => {
+  try {
+    const response = await axiosInstance.get(`api/products/brand/${brand}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`New error generated: ${error}`);
+  }
+};

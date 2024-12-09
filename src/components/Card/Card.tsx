913a98144ad603a073315a97ae3f40ba40f7ac4c
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-12-09 16:35:40 -0500

    Add props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,25 @@
-import img from "../../assets/GOORIN-BROS-PANTHER.webp";
 import { Link } from "react-router-dom";
 
-function Card() {
+interface CardProps {
+  name: string;
+  price: number;
+  imageUrl: string;
+  productId: string;
+}
+
+const Card = ({ name, price, imageUrl, productId }: CardProps) => {
   return (
     <div className="flex w-full max-w-44 flex-col gap-2 rounded-xl px-2 py-4 shadow-lg sm:max-w-48">
-      <img src={img} alt="Image" />
+      <img
+        src={imageUrl}
+        alt={name}
+        className="h-40 w-full rounded-lg object-cover"
+      />
       <div className="flex flex-col items-center gap-1">
-        <h3 className="text-center text-sm font-bold">GOORIN BROS PANTHER</h3>
-        <p className="text-sm font-semibold">$ 220.000</p>
+        <h3 className="text-center text-sm font-bold">{name}</h3>
+        <p className="text-sm font-semibold">$ {price.toLocaleString()}</p>
         <Link
-          to={"/producto"}
+          to={`/producto/${productId}`}
           className="rounded-md bg-black px-4 py-1 text-white"
         >
           Ver más
@@ -17,6 +27,6 @@ function Card() {
       </div>
     </div>
   );
-}
+};
 
 export default Card;

8821b185afcb51d01cd3cd1a0f7bb7572eb2eae2
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-12-09 16:55:10 -0500

    Add get products by brand with params

--- src/Pages/Guest/Pages/Sections/Sections.tsx
+++ src/Pages/Guest/Pages/Sections/Sections.tsx
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
 import { Card } from "../../../../components";
+import { getProductByBrand } from "../../../../services";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Product } from "../../../../types";
 
 export function Sections() {
+  const { brand } = useParams();
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const productsData = await getProductByBrand(brand);
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error al cargar el producto:", error);
+      }
+    };
+
+    fetchProduct();
+  }, [brand]);
+
+  if (!products) return <p>Cargando...</p>;
+
   return (
     <section className="flex justify-center p-3 font-Poppins">
       <div className="w-full max-w-4xl">
@@ -13,10 +35,15 @@ export function Sections() {
         <div>
           <h2 className="text-2xl font-semibold">GOORIN BROS</h2>
           <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {products.map((product: Product) => (
+              <Card
+                key={product._id}
+                name={product.name}
+                price={product.price}
+                imageUrl={product.imagesUrl[0]} // Mostramos la primera imagen
+                productId={product._id}
+              />
+            ))}
           </div>
         </div>
       </div>

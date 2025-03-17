35c013637ccebe5a5f971e9cdbc7023544a29710
Juan Aguiar <jaguiarcruz17@gmail.com>
2025-03-16 20:46:02 -0500

    Add css styles

--- src/Pages/Guest/Pages/Login/Login.tsx
+++ src/Pages/Guest/Pages/Login/Login.tsx
@@ -37,28 +37,37 @@ export function Login() {
   return (
     <section className="flex h-96 w-full items-center justify-center">
       <div className="flex justify-center rounded-md border py-5 shadow-md shadow-gray-300 first:w-96">
-        <form onSubmit={onSubmit} className="flex w-fit flex-col gap-3">
-          <div className="flex flex-col gap-1">
+        <form
+          onSubmit={onSubmit}
+          className="flex w-fit flex-col items-center gap-3"
+        >
+          <h2 className="text-balance text-center text-2xl font-semibold">
+            Administracion CapFlow
+          </h2>
+          <div className="flex w-full flex-col gap-1">
             <label htmlFor="username">Nombre de usuario</label>
             <input
-              className="w-64 rounded border border-black px-2 py-1"
+              className="rounded border border-black px-2 py-1"
               type="text"
               id="username"
               ref={usernameRef}
               placeholder="Ingrese su nombre de usuario"
             />
           </div>
-          <div className="flex flex-col gap-1">
+          <div className="flex w-full flex-col gap-1">
             <label htmlFor="password">Contraseña</label>
             <input
-              className="w-64 rounded border border-black px-2 py-1"
+              className="rounded border border-black px-2 py-1"
               type="password"
               id="password"
               ref={passwordRef}
               placeholder="Ingrese su contraseña"
             />
           </div>
-          <button className="rounded bg-black py-1 text-white" type="submit">
+          <button
+            className="h-10 w-full rounded bg-black py-1 text-lg text-white"
+            type="submit"
+          >
             Ingresar
           </button>
         </form>
